9795ca6722755b0ba629790d1501fb744b803848
fr34kyn01535 <fr34kyn01535@bam.yt>
2016-03-11 15:33:17 +0100

    New version

--- Rocket.API/Properties/AssemblyInfo.cs
+++ Rocket.API/Properties/AssemblyInfo.cs
@@ -6,4 +6,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Rocket.API")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: Guid("8870d132-f877-4fbd-9e73-49c8b1af8b3f")]
-[assembly: AssemblyVersion("2.5.0.0")]
+[assembly: AssemblyVersion("2.6.0.0")]
