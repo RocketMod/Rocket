069fc895b8874e9024c9f67d2a54bd121f123fde
fr34kyn01535 <fr34kyn01535@bam.yt>
2016-09-11 14:27:42 +0200

    This one is for @Leo :P

--- Rocket.Core/Assets/WebXMLFileAsset.cs
+++ Rocket.Core/Assets/WebXMLFileAsset.cs
@@ -10,49 +10,15 @@ namespace Rocket.Core.Assets
     public class WebXMLFileAsset<T> : Asset<T> where T : class
     {
         private Uri url;
-        RocketWebClient webclient;
+        private RocketWebClient webclient = new RocketWebClient();
+        private System.Net.DownloadStringCompletedEventHandler handler = new System.Net.DownloadStringCompletedEventHandler((object sender, System.Net.DownloadStringCompletedEventArgs e) => { });
+        private XmlRootAttribute attr;
         private bool waiting = false;
 
         public WebXMLFileAsset(Uri url = null, XmlRootAttribute attr = null, AssetLoaded<T> callback = null)
         {
             this.url = url;
-            webclient = new RocketWebClient();
-            webclient.DownloadStringCompleted += (object sender, System.Net.DownloadStringCompletedEventArgs e) =>
-            {
-                if (e.Error != null)
-                {
-                    Logger.Log(String.Format("Error retrieving WebXMLFileAsset {0} from {1}: {2}", typeof(T).Name, url, e.Error.Message));
-                }
-                else
-                {
-                    try
-                    {
-                        using (StringReader reader = new StringReader(e.Result))
-                        {
-                            XmlSerializer serializer = new XmlSerializer(typeof(T), attr);
-                            T result = (T)serializer.Deserialize(reader);
-                            if (result != null)
-                                TaskDispatcher.QueueOnMainThread(() =>
-                                {
-                                    instance = result;
-                                    Logger.Log(String.Format("Successfully updated WebXMLFileAsset {0} from {1}", typeof(T).Name, url));
-                                });
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        Logger.Log(String.Format("Error retrieving WebXMLFileAsset {0} from {1}: {2}", typeof(T).Name, url,ex.Message));
-                    }
-                }
-
-                TaskDispatcher.QueueOnMainThread(() =>
-                {
-                    if (callback != null)
-                        callback(this);
-                    waiting = false;
-                });
-
-            };
+            this.attr = attr;
             Load(callback);
         }
 
@@ -64,6 +30,43 @@ namespace Rocket.Core.Assets
                 {
                     Logger.Log(String.Format("Updating WebXMLFileAsset {0} from {1}", typeof(T).Name, url));
                     waiting = true;
+
+                    webclient.DownloadStringCompleted -= handler;
+                    handler = (object sender, System.Net.DownloadStringCompletedEventArgs e) =>
+                    {
+                        if (e.Error != null)
+                        {
+                            Logger.Log(String.Format("Error retrieving WebXMLFileAsset {0} from {1}: {2}", typeof(T).Name, url, e.Error.Message));
+                        }
+                        else
+                        {
+                            try
+                            {
+                                using (StringReader reader = new StringReader(e.Result))
+                                {
+                                    XmlSerializer serializer = new XmlSerializer(typeof(T), attr);
+                                    T result = (T)serializer.Deserialize(reader);
+                                    if (result != null)
+                                        TaskDispatcher.QueueOnMainThread(() =>
+                                        {
+                                            instance = result;
+                                            Logger.Log(String.Format("Successfully updated WebXMLFileAsset {0} from {1}", typeof(T).Name, url));
+                                        });
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                Logger.Log(String.Format("Error retrieving WebXMLFileAsset {0} from {1}: {2}", typeof(T).Name, url, ex.Message));
+                            }
+                        }
+
+                        TaskDispatcher.QueueOnMainThread(() =>
+                        {
+                            callback?.Invoke(this);
+                            waiting = false;
+                        });
+                    };
+                    webclient.DownloadStringCompleted += handler;
                     webclient.DownloadStringAsync(url);
                 }
             }
