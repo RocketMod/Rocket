5e3e3380c0dbf6d2ae77d26009d18703ea281822
Sven Mawby <fr34kyn01535@bam.yt>
2018-03-30 23:57:25 +0200

    .

--- Rocket.Runtime/Properties/CommonAssemblyInfo.cs
+++ Rocket.Runtime/Properties/CommonAssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
