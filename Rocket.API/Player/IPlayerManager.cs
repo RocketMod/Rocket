4b07e525041bc6efa169f3da1745d59198c8ba01
ChubbyQuokka <graybad1@icloud.com>
2018-03-31 13:26:57 -0400

    Added more methods to IPlayerManager

--- Rocket.API/Player/IPlayerManager.cs
+++ Rocket.API/Player/IPlayerManager.cs
@@ -12,5 +12,11 @@ namespace Rocket.API.Player
         IEnumerable<IPlayer> Players { get; }
 
         IPlayer GetPlayer(string uniqueID);
+
+        IPlayer GetPlayerByName(string displayName);
+
+        bool TryGetPlayer(string uniqueID, out IPlayer output);
+
+        bool TryGetPlayerByName(string displayName, out IPlayer output);
     }
 }
