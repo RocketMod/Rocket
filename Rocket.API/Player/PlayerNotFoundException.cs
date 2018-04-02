eae3c56fe4ae9901e032d04ec78403bb2806a421
ChubbyQuokka <graybad1@icloud.com>
2018-04-01 22:31:53 -0400

    Added PlayerNotFoundException

---
+++ Rocket.API/Player/PlayerNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Rocket.API.Player
+{
+    public class PlayerNotFoundException : Exception
+    {
+        public PlayerNotFoundException() : base($"Could not find the requested player.") { }
+    }
+}
