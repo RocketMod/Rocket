22e68b111edb097884df27a94a7c3280e3c6afe8
Trojaner_ <trojaner25@gmail.com>
2018-04-05 21:38:18 +0300

    Add example plugin

---
+++ Examples/HelloWorldPlugin/HelloWorldPluginMain.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Rocket.API.DependencyInjection;
+using Rocket.API.Logging;
+using Rocket.Core.Plugins;
+
+namespace HelloWorldPlugin
+{
+    public class HelloWorldPluginMain : PluginBase
+    {
+        private readonly ILogger _logger;
+
+        public HelloWorldPluginMain(IDependencyContainer container, ILogger logger) : base("HelloWorldPlugin", container)
+        {
+            _logger = logger;
+        }
+
+        protected override void OnLoad()
+        {
+            _logger.Info("Hello world!");
+        }
+
+        protected override void OnUnload()
+        {
+
+        }
+
+        public override IEnumerable<string> Capabilities => new List<string>();
+    }
+}
