5e5ffd0727d191a9fab1573bd132ae04ba18bc13
Trojaner_ <trojaner25@gmail.com>
2018-04-12 01:14:26 +0300

    ImplementationReadyEvent

---
+++ Rocket.Core/Events/Implementation/ImplementationReadyEvent.cs
@@ -0,0 +1,30 @@
+﻿using Rocket.API;
+using Rocket.API.Eventing;
+
+namespace Rocket.Core.Events.Implementation
+{
+    public class ImplementationReadyEvent : Event
+    {
+        public IImplementation Implementation { get; }
+
+        protected ImplementationReadyEvent(IImplementation implementation) : this(implementation, true)
+        {
+
+        }
+
+        protected ImplementationReadyEvent(IImplementation implementation, bool global = true) : base(global)
+        {
+            Implementation = implementation;
+        }
+
+        protected ImplementationReadyEvent(IImplementation implementation, EventExecutionTargetContext executionTarget = EventExecutionTargetContext.Sync, bool global = true) : base(executionTarget, global)
+        {
+            Implementation = implementation;
+        }
+
+        protected ImplementationReadyEvent(IImplementation implementation, string name = null, EventExecutionTargetContext executionTarget = EventExecutionTargetContext.Sync, bool global = true) : base(name, executionTarget, global)
+        {
+            Implementation = implementation;
+        }
+    }
+}
