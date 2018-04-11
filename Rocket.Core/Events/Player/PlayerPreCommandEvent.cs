1fc61bf098199a816f8bc9225c60aec0d69bd6f5
Trojaner_ <trojaner25@gmail.com>
2018-04-12 01:34:29 +0300

    PlayerPreCommandEvent

---
+++ Rocket.Core/Events/Player/PlayerPreCommandEvent.cs
@@ -0,0 +1,26 @@
+﻿using Rocket.API.Eventing;
+using Rocket.API.Player;
+
+namespace Rocket.Core.Events.Player
+{
+    public class PlayerPreCommandEvent : PlayerEvent
+    {
+        public string CommandLine { get; }
+        public PlayerPreCommandEvent(IPlayer player, string commandLine) : base(player)
+        {
+            CommandLine = commandLine;
+        }
+        public PlayerPreCommandEvent(IPlayer player, string commandLine, bool global = true) : base(player, global)
+        {
+            CommandLine = commandLine;
+        }
+        public PlayerPreCommandEvent(IPlayer player, string commandLine, EventExecutionTargetContext executionTarget = EventExecutionTargetContext.Sync, bool global = true) : base(player, executionTarget, global)
+        {
+            CommandLine = commandLine;
+        }
+        public PlayerPreCommandEvent(IPlayer player, string commandLine, string name = null, EventExecutionTargetContext executionTarget = EventExecutionTargetContext.Sync, bool global = true) : base(player, name, executionTarget, global)
+        {
+            CommandLine = commandLine;
+        }
+    }
+}
