711f42056bf3694d8bbaccf3e9c6719fed733a7d
ChubbyQuokka <graybad1@icloud.com>
2018-04-12 18:56:20 -0400

    Made IConsoleCommandProvider bypass permissions

--- Rocket.Core/Commands/CommandHandler.cs
+++ Rocket.Core/Commands/CommandHandler.cs
@@ -29,27 +29,30 @@ namespace Rocket.Core.Commands
             if (target == null)
                 return false; // only return false when the command was not found
 
-            var tmp = new List<string>{ target.Name };
-            if(target.Permission != null)
-                tmp.Add(target.Permission);
-
-            var perms = tmp.ToArray();
-
-            var permProviders = container.GetHandlers<IPermissionProvider>();
-            permProviders.Reverse(); //usually Lowest gets called first, so we need to reverse order
-
-            bool hasPermission = false;
-            foreach (var provider in permProviders)
+            if (!(caller is IConsoleCommandCaller))
             {
-                if (provider.HasAnyPermissions(caller, perms))
-                {
-                    hasPermission = true;
-                    break;
-                }
-            }
+                var tmp = new List<string> { target.Name };
+                if (target.Permission != null)
+                    tmp.Add(target.Permission);
 
-            if(!hasPermission)
-                throw new NotEnoughPermissionsException(caller, perms);
+                var perms = tmp.ToArray();
+
+                var permProviders = container.GetHandlers<IPermissionProvider>();
+                permProviders.Reverse(); //usually Lowest gets called first, so we need to reverse order
+
+                bool hasPermission = false;
+                foreach (var provider in permProviders)
+                {
+                    if (provider.HasAnyPermissions(caller, perms))
+                    {
+                        hasPermission = true;
+                        break;
+                    }
+                }
+
+                if (!hasPermission)
+                    throw new NotEnoughPermissionsException(caller, perms);
+            }
 
             try
             {
