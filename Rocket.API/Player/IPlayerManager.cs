fba37fa8ea444c187ac27f5f6b924fd025a45adc
Trojaner_ <trojaner25@gmail.com>
2018-04-13 21:28:31 +0300

    More refactoring

--- Rocket.API/Player/IPlayerManager.cs
+++ Rocket.API/Player/IPlayerManager.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using Rocket.API.Commands;
 
 namespace Rocket.API.Player
 {
     public interface IPlayerManager
     {
         IEnumerable<IPlayer> Players { get; }
-        bool Kick(IPlayer player, IPlayer kicker = null, string reason = null);
+        bool Kick(IPlayer player, ICommandCaller kicker = null, string reason = null);
 
-        bool Ban(IPlayer player, IPlayer banner = null, string reason = null, TimeSpan? timeSpan = null);
+        bool Ban(IPlayer player, ICommandCaller banner = null, string reason = null, TimeSpan? timeSpan = null);
 
         IPlayer GetPlayer(string uniqueID);
 
--- Rocket.Core/Events/Player/PlayerBanEvent.cs
+++ Rocket.Core/Events/Player/PlayerBanEvent.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Rocket.API.Commands;
 using Rocket.API.Eventing;
 using Rocket.API.Player;
 
@@ -7,28 +8,28 @@ namespace Rocket.Core.Events.Player
     public class PlayerBanEvent : PlayerEvent, ICancellableEvent
     {
         public TimeSpan? Duration { get; set; }
-        public IPlayer Banner { get; }
+        public ICommandCaller Banner { get; }
         public string Reason { get; set; }
 
-        public PlayerBanEvent(IPlayer player, IPlayer banner = null, string reason = null, TimeSpan? duration = null) : base(player)
+        public PlayerBanEvent(IPlayer player, ICommandCaller banner = null, string reason = null, TimeSpan? duration = null) : base(player)
         {
             Banner = banner;
             Reason = reason;
             Duration = duration;
         }
-        public PlayerBanEvent(IPlayer player, IPlayer banner = null, string reason = null, TimeSpan? duration = null, bool global = true) : base(player, global)
+        public PlayerBanEvent(IPlayer player, ICommandCaller banner = null, string reason = null, TimeSpan? duration = null, bool global = true) : base(player, global)
         {
             Banner = banner;
             Reason = reason;
             Duration = duration;
         }
-        public PlayerBanEvent(IPlayer player, IPlayer banner = null, string reason = null, TimeSpan? duration = null, EventExecutionTargetContext executionTarget = EventExecutionTargetContext.Sync, bool global = true) : base(player, executionTarget, global)
+        public PlayerBanEvent(IPlayer player, ICommandCaller banner = null, string reason = null, TimeSpan? duration = null, EventExecutionTargetContext executionTarget = EventExecutionTargetContext.Sync, bool global = true) : base(player, executionTarget, global)
         {
             Banner = banner;
             Reason = reason;
             Duration = duration;
         }
-        public PlayerBanEvent(IPlayer player, IPlayer banner = null, string reason = null, TimeSpan? duration = null, string name = null, EventExecutionTargetContext executionTarget = EventExecutionTargetContext.Sync, bool global = true) : base(player, name, executionTarget, global)
+        public PlayerBanEvent(IPlayer player, ICommandCaller banner = null, string reason = null, TimeSpan? duration = null, string name = null, EventExecutionTargetContext executionTarget = EventExecutionTargetContext.Sync, bool global = true) : base(player, name, executionTarget, global)
         {
             Banner = banner;
             Reason = reason;
