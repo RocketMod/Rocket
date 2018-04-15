8fc06411c701c9e072f32091a046e442584a0a84
Trojaner_ <trojaner25@gmail.com>
2018-04-15 23:56:01 +0300

    Add ConsoleColor to Loggers

--- Rocket.API/Logging/ILogger.cs
+++ Rocket.API/Logging/ILogger.cs
@@ -10,17 +10,41 @@ namespace Rocket.API.Logging
         bool IsWarnEnabled { get; }
         bool IsErrorEnabled { get; }
         bool IsFatalEnabled { get; }
+
         void LogTrace(string message, params object[] arguments);
+        void LogTrace(string message, ConsoleColor? color, params object[] arguments);
+
         void LogTrace(string message, Exception exception, params object[] arguments);
+        void LogTrace(string message, Exception exception, ConsoleColor? color, params object[] arguments);
+
         void LogDebug(string message, params object[] arguments);
+        void LogDebug(string message, ConsoleColor? color, params object[] arguments);
+
         void LogDebug(string message, Exception exception, params object[] arguments);
+        void LogDebug(string message, Exception exception, ConsoleColor? color, params object[] arguments);
+
         void LogInformation(string message, params object[] arguments);
+        void LogInformation(string message, ConsoleColor? color, params object[] arguments);
+
         void LogInformation(string message, Exception exception, params object[] arguments);
+        void LogInformation(string message, Exception exception, ConsoleColor? color, params object[] arguments);
+
         void LogWarning(string message, params object[] arguments);
+        void LogWarning(string message, ConsoleColor? color, params object[] arguments);
+
         void LogWarning(string message, Exception exception, params object[] arguments);
+        void LogWarning(string message, Exception exception, ConsoleColor? color, params object[] arguments);
+
         void LogError(string message, params object[] arguments);
+        void LogError(string message, ConsoleColor? color, params object[] arguments);
+
         void LogError(string message, Exception exception, params object[] arguments);
+        void LogError(string message, Exception exception, ConsoleColor? color = null, params object[] arguments);
+
         void LogFatal(string message, params object[] arguments);
+        void LogFatal(string message, ConsoleColor? color, params object[] arguments);
+
         void LogFatal(string message, Exception exception, params object[] arguments);
+        void LogFatal(string message, Exception exception, ConsoleColor? color, params object[] arguments);
     }
 }
--- Rocket.Core/Logging/ConsoleLogger.cs
+++ Rocket.Core/Logging/ConsoleLogger.cs
@@ -46,34 +46,59 @@ namespace Rocket.Core.Logging
 
         public void LogDebug(string message, params object[] arguments)
         {
+            throw new NotImplementedException();
+        }
+
+        public void LogDebug(string message, ConsoleColor? color, params object[] arguments)
+        {
             ConsoleColor orgCol = Console.ForegroundColor;
-            SetColor(ConsoleColor.Cyan);
+            SetColor(color ?? ConsoleColor.Cyan);
             Console.WriteLine($"{debugPrefix} {message}", arguments);
             SetColor(orgCol);
         }
 
         public void LogDebug(string message, Exception exception, params object[] arguments)
         {
+            LogDebug(message, exception, null, arguments);
+        }
+
+        public void LogDebug(string message, Exception exception, ConsoleColor? color, params object[] arguments)
+        {
             ConsoleColor orgCol = Console.ForegroundColor;
-            SetColor(ConsoleColor.Cyan);
+            SetColor(color ?? ConsoleColor.Cyan);
             Console.WriteLine($"{debugPrefix} {message}", arguments);
             SetColor(ConsoleColor.Red);
             Console.WriteLine(exception);
             SetColor(orgCol);
         }
 
+        public void LogInformation(string message, params object[] arguments)
+        {
+            LogInformation(message, (ConsoleColor?)null, arguments);
+        }
+
         public void LogError(string message, params object[] arguments)
         {
+            LogError(message, (ConsoleColor?)null, arguments);
+        }
+
+        public void LogError(string message, ConsoleColor? color, params object[] arguments)
+        {
             ConsoleColor orgCol = Console.ForegroundColor;
-            SetColor(ConsoleColor.Red);
+            SetColor(color ?? ConsoleColor.Red);
             Console.WriteLine($"{errorPrefix} {message}", arguments);
             SetColor(orgCol);
         }
 
         public void LogError(string message, Exception exception, params object[] arguments)
         {
+            LogError(message, (ConsoleColor?)null, arguments);
+        }
+
+        public void LogError(string message, Exception exception, ConsoleColor? color, params object[] arguments)
+        {
             ConsoleColor orgCol = Console.ForegroundColor;
-            SetColor(ConsoleColor.Red);
+            SetColor(color ?? ConsoleColor.Red);
             Console.WriteLine($"{errorPrefix} {message}", arguments);
             SetColor(ConsoleColor.Red);
             Console.WriteLine(exception);
@@ -82,70 +107,105 @@ namespace Rocket.Core.Logging
 
         public void LogFatal(string message, params object[] arguments)
         {
+            LogFatal(message, (ConsoleColor?)null, arguments);
+        }
+
+        public void LogFatal(string message, ConsoleColor? color, params object[] arguments)
+        {
             ConsoleColor orgCol = Console.ForegroundColor;
-            SetColor(ConsoleColor.Red);
+            SetColor(color ?? ConsoleColor.Red);
             Console.WriteLine($"{fatalPrefix} {message}", arguments);
             SetColor(orgCol);
         }
 
         public void LogFatal(string message, Exception exception, params object[] arguments)
         {
+            LogFatal(message, (ConsoleColor?)null, arguments);
+        }
+
+        public void LogFatal(string message, Exception exception, ConsoleColor? color, params object[] arguments)
+        {
             ConsoleColor orgCol = Console.ForegroundColor;
-            SetColor(ConsoleColor.Red);
+            SetColor(color ?? ConsoleColor.Red);
             Console.WriteLine($"{fatalPrefix} {message}", arguments);
             SetColor(ConsoleColor.Red);
             Console.WriteLine(exception);
             SetColor(orgCol);
         }
 
-        public void LogInformation(string message, params object[] arguments)
+        public void LogInformation(string message, ConsoleColor? color, params object[] arguments)
         {
             ConsoleColor orgCol = Console.ForegroundColor;
-            SetColor(ConsoleColor.White);
+            SetColor(color ?? ConsoleColor.White);
             Console.WriteLine($"{infoPrefix} {message}", arguments);
             SetColor(orgCol);
         }
 
         public void LogInformation(string message, Exception exception, params object[] arguments)
         {
+            LogInformation(message, exception, null, arguments);
+        }
+
+        public void LogInformation(string message, Exception exception, ConsoleColor? color, params object[] arguments)
+        {
             ConsoleColor orgCol = Console.ForegroundColor;
-            SetColor(ConsoleColor.White);
+            SetColor(color ?? ConsoleColor.White);
             Console.WriteLine($"{fatalPrefix} {message}", arguments);
             SetColor(ConsoleColor.Red);
             Console.WriteLine(infoPrefix);
             SetColor(orgCol);
         }
 
+        public void LogWarning(string message, params object[] arguments)
+        {
+            LogWarning(message, (ConsoleColor?)null, arguments);
+        }
+
         public void LogTrace(string message, params object[] arguments)
         {
+            LogTrace(message, (ConsoleColor?)null, arguments);
+        }
+
+        public void LogTrace(string message, ConsoleColor? color, params object[] arguments)
+        {
             ConsoleColor orgCol = Console.ForegroundColor;
-            SetColor(ConsoleColor.Cyan);
+            SetColor(color ?? ConsoleColor.Cyan);
             Console.WriteLine($"{tracePrefix} {message}", arguments);
             SetColor(orgCol);
         }
 
         public void LogTrace(string message, Exception exception, params object[] arguments)
         {
+            LogDebug(message, exception, null, arguments);
+        }
+
+        public void LogTrace(string message, Exception exception, ConsoleColor? color, params object[] arguments)
+        {
             ConsoleColor orgCol = Console.ForegroundColor;
-            SetColor(ConsoleColor.Cyan);
+            SetColor(color ?? ConsoleColor.Cyan);
             Console.WriteLine($"{tracePrefix} {message}", arguments);
             SetColor(ConsoleColor.Red);
             Console.WriteLine(exception);
             SetColor(orgCol);
         }
 
-        public void LogWarning(string message, params object[] arguments)
+        public void LogWarning(string message, ConsoleColor? color, params object[] arguments)
         {
             ConsoleColor orgCol = Console.ForegroundColor;
-            SetColor(ConsoleColor.Yellow);
+            SetColor(color ?? ConsoleColor.Yellow);
             Console.WriteLine($"{warnPrefix} {message}", arguments);
             SetColor(orgCol);
         }
 
         public void LogWarning(string message, Exception exception, params object[] arguments)
         {
+            LogWarning(message, exception, null, arguments);
+        }
+
+        public void LogWarning(string message, Exception exception, ConsoleColor? color, params object[] arguments)
+        {
             ConsoleColor orgCol = Console.ForegroundColor;
-            SetColor(ConsoleColor.Yellow);
+            SetColor(color ?? ConsoleColor.Yellow);
             Console.WriteLine($"{tracePrefix} {message}", arguments);
             SetColor(ConsoleColor.Red);
             Console.WriteLine(warnPrefix);
