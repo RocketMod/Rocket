821db26382fa2313fcf8291e8b2230dcb314e743
Trojaner_ <trojaner25@gmail.com>
2018-04-16 21:13:33 +0300

    Add EconomyProvider

---
+++ Rocket.API/Economy/EconomyProvider.cs
@@ -0,0 +1,15 @@
+﻿using Rocket.API.Commands;
+
+namespace Rocket.API.Economy
+{
+    public interface EconomyProvider
+    {
+        decimal GetBalance(ICommandCaller caller);
+
+        void AddBalance(ICommandCaller caller, decimal amount, string reason = null);
+
+        bool RemoveBalance(ICommandCaller caller, decimal amount, string reason = null);
+
+        void SetBalance(ICommandCaller caller, decimal amount);
+    }
+}
