8e0208fda7bfe4022d2a40b16b1d97595cc5bc86
Trojaner_ <trojaner25@gmail.com>
2018-04-27 04:00:44 +0300

    Add PlayerRespawnEvent

---
+++ Rocket.Core/Player/Events/PlayerRespawnEvent.cs
@@ -0,0 +1,13 @@
+﻿using Rocket.API.Eventing;
+using Rocket.API.Player;
+
+namespace Rocket.Core.Player.Events
+{
+    public class PlayerRespawnEvent : OnlinePlayerEvent
+    {
+        public PlayerRespawnEvent(IOnlinePlayer player) : base(player) { }
+        public PlayerRespawnEvent(IOnlinePlayer player, bool global = true) : base(player, global) { }
+        public PlayerRespawnEvent(IOnlinePlayer player, EventExecutionTargetContext executionTarget = EventExecutionTargetContext.Sync, bool global = true) : base(player, executionTarget, global) { }
+        public PlayerRespawnEvent(IOnlinePlayer player, string name = null, EventExecutionTargetContext executionTarget = EventExecutionTargetContext.Sync, bool global = true) : base(player, name, executionTarget, global) { }
+    }
+}
