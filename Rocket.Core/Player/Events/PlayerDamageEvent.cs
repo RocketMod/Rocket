66da29e16a2e9d88719b6df2e50a9773286d260c
Trojaner_ <trojaner25@gmail.com>
2018-04-27 04:12:55 +0300

    Add PlayerDamageEvent

---
+++ Rocket.Core/Player/Events/PlayerDamageEvent.cs
@@ -0,0 +1,36 @@
+﻿using Rocket.API.Commands;
+using Rocket.API.Eventing;
+using Rocket.API.Player;
+
+namespace Rocket.Core.Player.Events
+{
+    public class PlayerDamageEvent : OnlinePlayerEvent, ICancellableEvent
+    {
+        public ICommandCaller DamageDealer { get; }
+        public double Damage { get; }
+
+        public PlayerDamageEvent(IOnlinePlayer player, double damage, ICommandCaller damageDealer) : base(player)
+        {
+            Damage = damage;
+            DamageDealer = damageDealer;
+        }
+        public PlayerDamageEvent(IOnlinePlayer player, double damage, ICommandCaller damageDealer, bool global = true) : base(player, global)
+        {
+            Damage = damage;
+
+            DamageDealer = damageDealer;
+        }
+        public PlayerDamageEvent(IOnlinePlayer player, double damage, ICommandCaller damageDealer, EventExecutionTargetContext executionTarget = EventExecutionTargetContext.Sync, bool global = true) : base(player, executionTarget, global)
+        {
+            Damage = damage;
+            DamageDealer = damageDealer;
+        }
+        public PlayerDamageEvent(IOnlinePlayer player, double damage, ICommandCaller damageDealer, string name = null, EventExecutionTargetContext executionTarget = EventExecutionTargetContext.Sync, bool global = true) : base(player, name, executionTarget, global)
+        {
+            Damage = damage;
+            DamageDealer = damageDealer;
+        }
+
+        public bool IsCancelled { get; set; }
+    }
+}
