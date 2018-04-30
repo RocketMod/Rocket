e8925d7da043cd44677391fdb3c70cdf4f4f8cb4
Trojaner_ <trojaner25@gmail.com>
2018-04-30 16:51:24 +0300

    Service proxy documentation

--- Rocket.API/ServiceProxies/IProxyableService.cs
+++ Rocket.API/ServiceProxies/IProxyableService.cs
@@ -1,4 +1,7 @@
 ﻿namespace Rocket.API.ServiceProxies
 {
+    /// <summary>
+    ///     Defines a service which can be proxied.
+    /// </summary>
     public interface IProxyableService { }
 }
--- Rocket.API/ServiceProxies/IServiceProxy.cs
+++ Rocket.API/ServiceProxies/IServiceProxy.cs
@@ -2,10 +2,33 @@
 
 namespace Rocket.API.ServiceProxies
 {
+    /// <summary>
+    ///     <inheritdoc/><br/><br/>
+    ///     <b>Must deriver from <i>T</i></b>.
+    /// </summary>
+    /// <remarks>
+    ///     <inheritdoc/>
+    /// </remarks>
+    /// <typeparam name="T">The service to proxy (e.g. IChatManager).</typeparam>
     public interface IServiceProxy<T> : IServiceProxy where T : IProxyableService
     {
+        /// <summary>q
+        ///     All service instances which were proxied.
+        /// </summary>
         IEnumerable<T> ProxiedServices { get; }
     }
 
+    /// <summary>
+    ///     Defines a proxy service implementation.
+    /// </summary>
+    /// <remarks>
+    ///     A service proxy allows to have multiple implementations for the same service. For example,
+    ///     we can have multiple implementations for <i>IPermissionProvider</i>. The proxy allows to do this:
+    ///     <code>
+    ///         bool hasPermission = container.Get&lt;IPermissionProvider&gt;().CheckPermission(player, "mypermission") == PermissionResult.Grant;
+    ///     </code>
+    ///     It looks like the code is only calling one permission provider, but in reality it is calling the the proxy which is calling all of the registered permission providers.<br/>
+    ///     This allows us to avoid having issues with load order of providers and prevents to do always foreach with container.GetAll&lt;&gt;. 
+    /// </remarks>
     public interface IServiceProxy { }
 }
