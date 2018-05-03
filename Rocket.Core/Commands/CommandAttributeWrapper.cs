bae90cdb0417637c0aacf3200ce77f61d88867f2
Trojaner_ <trojaner25@gmail.com>
2018-05-03 22:10:32 +0300

    Auto generate name and syntax from attribute commands

--- Rocket.Core/Commands/CommandAttributeWrapper.cs
+++ Rocket.Core/Commands/CommandAttributeWrapper.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Reflection;
 using Rocket.API.Commands;
 using Rocket.API.DependencyInjection;
+using Rocket.Core.DependencyInjection;
 
 namespace Rocket.Core.Commands
 {
+    [DontAutoRegister]
     public class CommandAttributeWrapper : ICommand
     {
         private readonly Type[] supportedCallers;
@@ -20,17 +22,34 @@ namespace Rocket.Core.Commands
             Instance = instance;
             Method = method;
             Attribute = attribute;
+            Name = attribute?.Name ?? method.Name;
+            Syntax = attribute?.Syntax ?? BuildSyntaxFromMethod();
+
             Aliases = aliases;
+        }
+
+        private string BuildSyntaxFromMethod()
+        {
+            List<ParameterInfo> parameters = (from param in Method.GetParameters()
+                                              let type = param.ParameterType
+                                              where type != typeof(ICommandContext)
+                                              where type != typeof(ICommandCaller)
+                                              where type != typeof(string[])
+                                              where type != typeof(ICommandParameters)
+                                              where type != typeof(IDependencyContainer)
+                                              select param).ToList();
+
+            return string.Join(" ", parameters.Select(c => $"<{c.Name}>").ToArray());
         }
 
         public CommandAttribute Attribute { get; }
         public object Instance { get; }
         public MethodBase Method { get; set; }
 
-        public string Name => Attribute.Name;
-        public string Description => Attribute.Description;
-        public string Permission => Attribute.Permission;
-        public string Syntax => Attribute.Syntax;
+        public string Name { get; }
+        public string Description => Attribute?.Description;
+        public string Permission => Attribute?.Permission;
+        public string Syntax { get; }
         public ISubCommand[] ChildCommands { get; } //todo
         public string[] Aliases { get; }
 
@@ -76,7 +95,14 @@ namespace Rocket.Core.Commands
                 }
                 else
                 {
-                    @params.Add(context.Parameters.Get(index, type));
+                    try
+                    {
+                        @params.Add(context.Parameters.Get(index, type));
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        throw new CommandWrongUsageException();
+                    }
                     index++;
                 }
             }
