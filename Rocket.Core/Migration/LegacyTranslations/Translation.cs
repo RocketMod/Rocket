e3d2f8fe1267319f9a76397f930844e448e06fea
Trojaner_ <trojaner25@gmail.com>
2018-05-07 15:37:10 +0300

    Add missing legacy translation class

---
+++ Rocket.Core/Migration/LegacyTranslations/Translation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Rocket.Core.Migration.LegacyTranslations
+{
+    public class Translation
+    {
+        public string Id { get; set; }
+
+        public string Value { get; set; }
+    }
+}
