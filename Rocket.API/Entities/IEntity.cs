9bbe6aa4ae65063624fcf5c8f2e59285250eee70
Trojaner_ <trojaner25@gmail.com>
2018-05-15 14:25:48 +0300

    Add Entitiy.Position

--- Rocket.API/Entities/IEntity.cs
+++ Rocket.API/Entities/IEntity.cs
@@ -1,4 +1,6 @@
-﻿namespace Rocket.API.Entities
+﻿using Rocket.API.Math;
+
+namespace Rocket.API.Entities
 {
     /// <summary>
     ///     A game entitiy. Players, Monsters, Animals, Vehicles, Zombies, etc...
@@ -9,5 +11,10 @@
         ///     The type name of the entitiy. "Zombie", "Player", "Animal", etc...
         /// </summary>
         string EntityTypeName { get; }
+
+        /// <summary>
+        ///     The world position of the entity.
+        /// </summary>
+        Vector3 Position { get; }
     }
 }
