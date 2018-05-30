0b0dc11294fc400b0748274ef20681462fed3430
Trojaner_ <trojaner25@gmail.com>
2018-05-31 01:45:12 +0300

    Add util methods to get Colors from name / string / hex

---
+++ Rocket.Core/Util/ColorUtility.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+
+namespace Rocket.Core.Util
+{
+    public class ColorUtility
+    {
+        public Color? GetColorFromString(string colorString)
+        {
+            return GetColorFromName(colorString) ?? GetColorFromHex(colorString);
+        }
+
+        public Color? GetColorFromHex(string colorHex)
+        {
+            if (int.TryParse(colorHex.Replace("#", ""), NumberStyles.HexNumber, null, out int argb))
+            {
+                return Color.FromArgb(argb);
+            }
+
+            return null;
+        }
+
+        public Color? GetColorFromName(string colorName)
+        {
+            var color = Color.FromName(colorName);
+            return color == default(Color) ? (Color?) null : color;
+        }
+    }
+}
