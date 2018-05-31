3f3d16bc05683740b9b2a5012ae39f2f889d36d5
Aleksei Ivanov <alekaei522@gmail.com>
2018-06-01 00:38:31 +0100

    Added requested changes.

--- Rocket.Core/Logging/ConsoleLogger.cs
+++ Rocket.Core/Logging/ConsoleLogger.cs
@@ -37,7 +37,7 @@ namespace Rocket.Core.Logging
             }
 
             SetForegroundColor(Color.White);
-            Console.WriteLine(String.Format (message, arguments));
+            Console.WriteLine(message, arguments);
 
             if (exception != null)
             {
