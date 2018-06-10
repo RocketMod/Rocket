ce4d1295dd482a43a012efc1cf9e1e1654e1ebcf
Trojaner_ <trojaner25@gmail.com>
2018-06-10 20:55:22 +0300

    [IB] (Plugins have to recompile) Simplify ITaskScheduler. Closes #1084.

--- Rocket.API/Scheduler/ITaskScheduler.cs
+++ Rocket.API/Scheduler/ITaskScheduler.cs
@@ -15,22 +15,6 @@ namespace Rocket.API.Scheduler
         IEnumerable<ITask> Tasks { get; }
 
         /// <summary>
-        ///     Schedules an action for all frames on the main thread. See <see cref="ExecutionTargetContext.EveryFrame" />.
-        /// </summary>
-        /// <param name="action">The action to schedule. Must not block the thread.</param>
-        /// <param name="owner">The owner of the task.</param>
-        /// <param name="taskName">The tasks human friendly name.</param>
-        ITask ScheduleEveryFrame(ILifecycleObject owner, Action action, string taskName);
-
-        /// <summary>
-        ///     Schedules an action for the next frame on the main thread. See <see cref="ExecutionTargetContext.NextFrame" />.
-        /// </summary>
-        /// <param name="action">The action to schedule. Must not block the thread.</param>
-        /// <param name="owner">The owner of the task.</param> 
-        /// <param name="taskName">The tasks human friendly name.</param> 
-        ITask ScheduleNextFrame(ILifecycleObject owner, Action action, string taskName);
-
-        /// <summary>
         ///     Schedules an action.
         /// </summary>
         /// <seealso cref="ExecutionTargetContext" />
@@ -41,56 +25,9 @@ namespace Rocket.API.Scheduler
         ITask ScheduleUpdate(ILifecycleObject owner, Action action, string taskName, ExecutionTargetContext target);
 
         /// <summary>
-        ///     Schedule an action which includes physics interactions (e.g. applying force to an object) for the next physics
-        ///     update. See <see cref="ExecutionTargetContext.NextPhysicsUpdate" />.<br />
-        ///     Execution time depends on the engine implemention.
-        /// </summary>
-        /// <param name="action">The action to schedule. Must not block the thread.</param>
-        /// <param name="owner">The owner of the task.</param>
-        /// <param name="taskName">The tasks human friendly name.</param>
-        ITask ScheduleNextPhysicUpdate(ILifecycleObject owner, Action action, string taskName);
-
-        /// <summary>
-        ///     Schedule an action which includes physics interaction (e.g. applying force to an object) for every physics update.
-        ///     See <see cref="ExecutionTargetContext.EveryPhysicsUpdate" />.<br />
-        ///     Execution time depends on the engine implemention.
-        /// </summary>
-        /// <param name="action">The action to schedule. Must not block the thread.</param>
-        /// <param name="owner">The owner of the task.</param>
-        /// <param name="taskName">The tasks human friendly name.</param>
-        ITask ScheduleEveryPhysicUpdate(ILifecycleObject owner, Action action, string taskName);
-
-        /// <summary>
-        ///     Schedules an action for all frame on a separate thread. See <see cref="ExecutionTargetContext.EveryAsyncFrame" />.
-        /// </summary>
-        /// <param name="action">The action to schedule. Must not block the thread.</param>
-        /// <param name="owner">The owner of the task.</param>
-        /// <param name="taskName">The tasks human friendly name.</param>
-        ITask ScheduleEveryAsyncFrame(ILifecycleObject owner, Action action, string taskName);
-
-        /// <summary>
-        ///     Schedules an action for the next frame on a separate thread. See
-        ///     <see cref="ExecutionTargetContext.NextPhysicsUpdate" />.
-        /// </summary>
-        /// <param name="action">The action to schedule. Must not block the thread.</param>
-        /// <param name="owner">The owner of the task.</param>
-        /// <param name="taskName">The tasks human friendly name.</param>
-        ITask ScheduleNextAsyncFrame(ILifecycleObject owner, Action action, string taskName);
-
-        /// <summary>
         ///     Cancels a task. Tasks are automatically cancelled on a plugin unload.
         /// </summary>
         bool CancelTask(ITask task);
-
-        /// <summary>
-        ///     Executes the given task roughly after the delay.
-        /// </summary>
-        /// <param name="object">The owner of the task.</param>
-        /// <param name="action">The action to schedule. Must not block thread if <i>runAsync</i> equals <b>false</b>.</param>
-        /// <param name="delay">The delay.</param>
-        /// <param name="runAsync">Defines if the task should run in a separate thread.</param>
-        /// <param name="taskName">The tasks human friendly name.</param>
-        ITask ScheduleDelayed(ILifecycleObject @object, Action action, string taskName, TimeSpan delay, bool runAsync = false);
 
         /// <summary>
         ///     Executes the given task roughly at the given time. Runs on the main thread if one exists. Note that after restarts the tasks may not be preserved.
---
+++ Rocket.Core/Scheduler/SchedulerExtensions.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Rocket.API;
+using Rocket.API.Scheduler;
+
+namespace Rocket.Core.Scheduler
+{
+    public static class SchedulerExtensions
+    {        
+        /// <summary>
+        ///     Schedules an action for the next frame on the main thread. See <see cref="ExecutionTargetContext.NextFrame" />.
+        /// </summary>
+        /// <param name="action">The action to schedule. Must not block the thread.</param>
+        /// <param name="owner">The owner of the task.</param> 
+        /// <param name="taskName">The tasks human friendly name.</param> 
+        public static ITask ScheduleNextFrame(this ITaskScheduler taskScheduler, ILifecycleObject @owner, Action action, string taskName) 
+            => taskScheduler.ScheduleUpdate(@owner, action, taskName, ExecutionTargetContext.NextFrame);
+
+        /// <summary>
+        ///     Schedules an action for all frames on the main thread. See <see cref="ExecutionTargetContext.EveryFrame" />.
+        /// </summary>
+        /// <param name="action">The action to schedule. Must not block the thread.</param>
+        /// <param name="owner">The owner of the task.</param>
+        /// <param name="taskName">The tasks human friendly name.</param>
+        public static ITask ScheduleEveryFrame(this ITaskScheduler taskScheduler, ILifecycleObject @owner, Action action, string taskName) 
+            => taskScheduler.ScheduleUpdate(@owner, action, taskName, ExecutionTargetContext.EveryFrame);
+
+        /// <summary>
+        ///     Schedule an action which includes physics interactions (e.g. applying force to an object) for the next physics
+        ///     update. See <see cref="ExecutionTargetContext.NextPhysicsUpdate" />.<br />
+        ///     Execution time depends on the engine implemention.
+        /// </summary>
+        /// <param name="action">The action to schedule. Must not block the thread.</param>
+        /// <param name="owner">The owner of the task.</param>
+        /// <param name="taskName">The tasks human friendly name.</param>
+        public static ITask ScheduleNextPhysicUpdate(this ITaskScheduler taskScheduler, ILifecycleObject @owner, Action action, string taskName) 
+            => taskScheduler.ScheduleUpdate(@owner, action, taskName, ExecutionTargetContext.NextPhysicsUpdate);
+
+        /// <summary>
+        ///     Schedule an action which includes physics interaction (e.g. applying force to an object) for every physics update.
+        ///     See <see cref="ExecutionTargetContext.EveryPhysicsUpdate" />.<br />
+        ///     Execution time depends on the engine implemention.
+        /// </summary>
+        /// <param name="action">The action to schedule. Must not block the thread.</param>
+        /// <param name="owner">The owner of the task.</param>
+        /// <param name="taskName">The tasks human friendly name.</param>
+        public static ITask ScheduleEveryPhysicUpdate(this ITaskScheduler taskScheduler, ILifecycleObject @owner, Action action, string taskName) 
+            => taskScheduler.ScheduleUpdate(@owner, action, taskName, ExecutionTargetContext.EveryPhysicsUpdate);
+
+        /// <summary>
+        ///     Schedules an action for all frame on a separate thread. See <see cref="ExecutionTargetContext.EveryAsyncFrame" />.
+        /// </summary>
+        /// <param name="action">The action to schedule. Must not block the thread.</param>
+        /// <param name="owner">The owner of the task.</param>
+        /// <param name="taskName">The tasks human friendly name.</param>
+        public static ITask ScheduleNextAsyncFrame(this ITaskScheduler taskScheduler, ILifecycleObject @owner, Action action, string taskName) 
+            => taskScheduler.ScheduleUpdate(@owner, action, taskName, ExecutionTargetContext.NextAsyncFrame);
+
+        /// <summary>
+        ///     Schedules an action for the next frame on a separate thread. See
+        ///     <see cref="ExecutionTargetContext.NextPhysicsUpdate" />.
+        /// </summary>
+        /// <param name="action">The action to schedule. Must not block the thread.</param>
+        /// <param name="owner">The owner of the task.</param>
+        /// <param name="taskName">The tasks human friendly name.</param>
+        public static ITask ScheduleEveryAsyncFrame(this ITaskScheduler taskScheduler, ILifecycleObject @object, Action action, string taskName) 
+            => taskScheduler.ScheduleUpdate(@object, action, taskName, ExecutionTargetContext.EveryAsyncFrame);
+
+        /// <summary>
+        ///     Executes the given task roughly after the given delay.
+        /// </summary>
+        /// <param name="object">The owner of the task.</param>
+        /// <param name="action">The action to schedule. Must not block thread if <i>runAsync</i> equals <b>false</b>.</param>
+        /// <param name="delay">The delay.</param>
+        /// <param name="runAsync">Defines if the task should run in a separate thread.</param>
+        /// <param name="taskName">The tasks human friendly name.</param>
+        public static ITask ScheduleDelayed(this ITaskScheduler taskScheduler, ILifecycleObject @object, Action action, string taskName, TimeSpan delay, bool runAsync = false)
+            => taskScheduler.ScheduleAt(@object, action, taskName, DateTime.Now + delay, runAsync);
+    }
+}
