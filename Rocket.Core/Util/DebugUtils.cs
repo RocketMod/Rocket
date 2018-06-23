42f66b3bcfd100487d2403306e8a421bd88a0cce
Trojaner_ <trojaner25@gmail.com>
2018-06-23 20:18:01 +0300

    Add WaitForDebugger

---
+++ Rocket.Core/Util/DebugUtils.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+namespace Rocket.Core.Util
+{
+    public class DebugUtils
+    {
+        public static void WaitForDebugger(bool autoBreak = true)
+        {
+            while (!Debugger.IsAttached)
+                Thread.Sleep(500);
+
+            if(autoBreak)
+                Debugger.Break();
+        }
+    }
+}
