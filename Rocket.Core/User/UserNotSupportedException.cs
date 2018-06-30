6bbefcadb3ace2ae0eeddc2e0a00ebed707fc4d3
Trojaner_ <trojaner25@gmail.com>
2018-06-30 12:40:38 +0300

    Added UserNotSupportedException. Closes #1095.

---
+++ Rocket.Core/User/UserNotSupportedException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Rocket.API.Commands;
+using Rocket.API.Drawing;
+
+namespace Rocket.Core.User
+{
+    public class UserNotSupportedException : Exception, ICommandFriendlyException
+    {
+        public UserNotSupportedException() : base("You can not execute this action.")
+        {
+            
+        }
+        public UserNotSupportedException(string message) : base(message)
+        {
+            
+        }
+
+        public void SendErrorMessage(ICommandContext context)
+        {
+            context.User.SendMessage(Message, Color.Red);
+        }
+    }
+}
