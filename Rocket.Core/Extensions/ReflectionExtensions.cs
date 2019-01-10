caa5b8ed7b0e3ca72fbd226da7b36f07f8212a1f
Enes Sadık Özbek <es.ozbek@outlook.com>
2019-01-10 10:56:06 +0300

    Hotfix for getting async method names

--- Rocket.Core/Extensions/ReflectionExtensions.cs
+++ Rocket.Core/Extensions/ReflectionExtensions.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using Rocket.API;
 
 namespace Rocket.Core.Extensions
@@ -23,7 +24,7 @@ namespace Rocket.Core.Extensions
             return filter;
         }
 
-        public static MethodBase GetCallingMethod(Type[] skipTypes = null, MethodBase[] skipMethods = null)
+        public static MethodBase GetCallingMethod(Type[] skipTypes = null, MethodBase[] skipMethods = null, bool applyAsyncMethodPatch = true)
         {
             var skipList = new List<Type>(skipTypes ?? new Type[0]);
             skipList.Add(typeof(ReflectionExtensions));
@@ -34,13 +35,42 @@ namespace Rocket.Core.Extensions
             {
                 StackFrame frame = st.GetFrame(i);
                 var frameMethod = frame.GetMethod();
+                if(frameMethod == null)
+                    continue;
+
+                // Hot fix for async Task methods:
+                // If current frame method is called "MoveNext" and parent frame is from "AsyncMethodBuilderCore" type 
+                //   it's an async method wrapper, so we need to skip these two frames to get the original calling async method
+                // Tested on .NET Core 2.1; should be tested on full .NET and mono too
+                if (applyAsyncMethodPatch && frameMethod is MethodInfo currentMethodFrameInfo && currentMethodFrameInfo.Name == "MoveNext")
+                {
+                    var tmpIndex = i;
+                    var frameOriginal = frame;
+
+                    frame = st.GetFrame(++tmpIndex);
+                    frameMethod = frame.GetMethod();
+
+                    // Check parent frame - if its from AsyncMethodBuilderCore, its definitely an async Task
+                    if (frameMethod is MethodInfo parentFrameMethodInfo && parentFrameMethodInfo.DeclaringType?.Name == "AsyncMethodBuilderCore")
+                    {
+                        frame = st.GetFrame(++tmpIndex);
+                        frameMethod = frame.GetMethod();
+
+                        i = tmpIndex;
+                    }
+                    else
+                    {
+                        //Restore original frame
+                        frame = frameOriginal;
+                        frameMethod = frameOriginal.GetMethod();
+                    }
+                }
 
                 if (skipList.Any(c => c == frameMethod?.DeclaringType))
                     continue;
 
                 if (skipMethods?.Any(c => c == frameMethod) ?? false)
                     continue;
-
                 target = frame;
                 break;
             }
