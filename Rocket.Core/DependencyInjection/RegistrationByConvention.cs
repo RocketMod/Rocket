ed3509d85d793cb818050f23c31abd495c4e0e50
Enes Sadık Özbek <es.ozbek@outlook.com>
2019-02-27 19:01:53 +0300

    Update packages

--- Rocket.Core/DependencyInjection/RegistrationByConvention.cs
+++ Rocket.Core/DependencyInjection/RegistrationByConvention.cs
@@ -11,29 +11,47 @@ namespace Rocket.Core.DependencyInjection
 {
     public class RegistrationByConvention
     {
+        private readonly IDependencyContainer container;
+        private readonly IDependencyResolver resolver;
+        private readonly ILogger logger;
+        private readonly List<string> activatedAssemblies = new List<string>();
+
         public RegistrationByConvention(IDependencyContainer container, IDependencyResolver resolver, ILogger logger)
         {
-            AppDomain.CurrentDomain.AssemblyLoad += (sender, args) =>
-            {
-                foreach (Type type in args.LoadedAssembly.GetTypesWithInterface<IDependencyRegistrator>())
-                    ((IDependencyRegistrator) Activator.CreateInstance(type)).Register(container, resolver);
-            };
+            this.container = container;
+            this.resolver = resolver;
+            this.logger = logger;
+            AppDomain.CurrentDomain.AssemblyLoad += (sender, args) => RegisterAssembly(args.LoadedAssembly);
+
+            var coreAssembly = typeof(RegistrationByConvention).Assembly;
+            RegisterAssembly(coreAssembly);
 
             List<Assembly> assemblies = AppDomain.CurrentDomain.GetAssemblies().ToList();
-
             logger.LogTrace("Assemblies: ["
                 + string.Join(", ", assemblies.Select(c => c.GetName().Name).ToArray())
                 + "]");
 
             foreach (Assembly assembly in assemblies)
             {
-                logger?.LogTrace("Registering assembly: " + assembly.FullName);
-                foreach (Type type in assembly.GetTypesWithInterface<IDependencyRegistrator>())
-                {
-                    logger?.LogTrace("\tRegistering from IDependencyRegistrator: " + type.FullName);
-                    ((IDependencyRegistrator) Activator.CreateInstance(type)).Register(container, resolver);
-                }
+                RegisterAssembly(assembly);
             }
+        }
+
+        private void RegisterAssembly(Assembly assembly)
+        {
+            if (activatedAssemblies.Contains(assembly.FullName))
+            {
+                return;
+            }
+
+            logger?.LogTrace("Registering assembly: " + assembly.FullName);
+            foreach (Type type in assembly.GetTypesWithInterface<IDependencyRegistrator>())
+            {
+                logger?.LogTrace("\tRegistering from IDependencyRegistrator: " + type.FullName);
+                ((IDependencyRegistrator)Activator.CreateInstance(type)).Register(container, resolver);
+            }
+
+            activatedAssemblies.Add(assembly.FullName);
         }
     }
 }
