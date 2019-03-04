19f5e834ad43bd2bea276963447d5df8248f2fc9
Enes Sadık Özbek <es.ozbek@outlook.com>
2019-03-04 17:34:02 +0300

    Support multiple packageIds in dynamic bootstrapper

--- Rocket.Runtime/RocketDynamicBootstrapper.cs
+++ Rocket.Runtime/RocketDynamicBootstrapper.cs
@@ -3,6 +3,7 @@ using NuGet.Packaging.Core;
 using NuGet.Protocol.Core.Types;
 using Rocket.NuGet;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -12,7 +13,14 @@ namespace Rocket
     {
         private const string DefaultNugetRepository = "https://api.nuget.org/v3/index.json";
 
-        public async Task BootstrapAsync(string rocketFolder, string packageId, bool allowPrereleaseVersions = false, string repository = DefaultNugetRepository)
+        public async Task BootstrapAsync(string rocketFolder, string packageId,
+                                         bool allowPrereleaseVersions = false,
+                                         string repository = DefaultNugetRepository)
+        {
+            await BootstrapAsync(rocketFolder, new List<string> {packageId}, allowPrereleaseVersions, repository);
+        }
+
+        public async Task BootstrapAsync(string rocketFolder, IEnumerable<string> packageIds, bool allowPrereleaseVersions = false, string repository = DefaultNugetRepository)
         {
             rocketFolder = Path.GetFullPath(rocketFolder);
 
@@ -31,31 +39,38 @@ namespace Rocket
             }
 
             var nugetInstaller = new NuGetPackageManager(logger, packagesDirectory, new []{ repository });
-            PackageIdentity packageIdentity;
-            if (!await nugetInstaller.PackageExistsAsync(packageId))
-            {
-                var rocketPackage =
-                    await nugetInstaller.QueryPackageExactAsync(packageId, null, allowPrereleaseVersions);
 
-                Console.WriteLine($"Downloading {rocketPackage.Identity.Id} v{rocketPackage.Identity.Version} via NuGet, this might take a while...");
-                var installResult = await nugetInstaller.InstallAsync(rocketPackage.Identity, allowPrereleaseVersions);
-                if (installResult.Code != NuGetInstallCode.Success)
+            foreach (var packageId in packageIds)
+            {
+                PackageIdentity packageIdentity;
+                if (!await nugetInstaller.PackageExistsAsync(packageId))
                 {
-                    Console.WriteLine($"Downloading finished. Loading runtime for {rocketPackage.Identity.Id}.");
-                    return;
+                    var rocketPackage =
+                        await nugetInstaller.QueryPackageExactAsync(packageId, null, allowPrereleaseVersions);
+
+                    Console.WriteLine(
+                        $"Downloading {rocketPackage.Identity.Id} v{rocketPackage.Identity.Version} via NuGet, this might take a while...");
+                    var installResult =
+                        await nugetInstaller.InstallAsync(rocketPackage.Identity, allowPrereleaseVersions);
+                    if (installResult.Code != NuGetInstallCode.Success)
+                    {
+                        Console.WriteLine($"Downloading finished. Loading runtime for {rocketPackage.Identity.Id}.");
+                        return;
+                    }
+
+                    packageIdentity = installResult.Identity;
+                    Console.WriteLine($"Downloading finished.");
+                }
+                else
+                {
+                    packageIdentity = await nugetInstaller.GetLatestPackageIdentityAsync(packageId);
                 }
 
-                packageIdentity = installResult.Identity;
-                Console.WriteLine($"Downloading finished.");
-            }
-            else
-            {
-                packageIdentity = await nugetInstaller.GetLatestPackageIdentityAsync(packageId);
+                Console.WriteLine($"Loading runtime for {packageId}.");
+
+                await LoadPackageAsync(nugetInstaller, packageIdentity);
             }
 
-            Console.WriteLine($"Loading runtime for {packageId}.");
-         
-            await LoadPackageAsync(nugetInstaller, packageIdentity);
             await InitializeRuntimeAsync();
         }
 
